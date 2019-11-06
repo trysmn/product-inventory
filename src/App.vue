f92600ee169745248c0f1440a2ebe53f3f8982a9
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-11-06 15:14:59 +0000

    product-inventory: Create ProductInventory component with data for one product and delete HelloWorld component
    Trystan

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductInventory />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductInventory from './components/ProductInventory';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ProductInventory
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ProductInventory.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="product-inventory">
+        <h2>{{ product }} are in stock.</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductInventory",
+    data: () => {
+        return {
+            product: "Boots"
+        };
+    }
+}
+</script>
+
+<style scoped>
+#product-inventory {
+    text-align: center;
+}
+</style>
